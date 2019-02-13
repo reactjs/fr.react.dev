4fc6c4670b23b9906ee894d52e59ea486e436942
Jeremie Patonnier <jeremie.patonnier@gmail.com>
2019-02-13 11:10:55 +0100

    Update get-snapshot-before-update.js

--- examples/react-component-reference/get-snapshot-before-update.js
+++ examples/react-component-reference/get-snapshot-before-update.js
@@ -6,7 +6,7 @@ class ScrollingList extends React.Component {
 
   getSnapshotBeforeUpdate(prevProps, prevState) {
     // Sommes-nous en train d’ajouter de nouveaux éléments à la liste ?
-    // Sauvegardons la position de défilement pour le recaler plus tard.
+    // Sauvegardons la position de défilement pour la recaler plus tard.
     if (prevProps.list.length < this.props.list.length) {
       const list = this.listRef.current;
       return list.scrollHeight - list.scrollTop;
@@ -16,7 +16,7 @@ class ScrollingList extends React.Component {
 
   componentDidUpdate(prevProps, prevState, snapshot) {
     // Si nous avons une valeur sauvegardée, c’est que nous venons d’ajouter des
-    // éléments.  Ajustons le défilement pour que ces nouveaux éléments ne
+    // éléments. Ajustons le défilement pour que ces nouveaux éléments ne
     // décalent pas les anciens hors du champ de vision. (ici `snapshot` est la
     // valeur renvoyée par getSnapshotBeforeUpdate.)
     if (snapshot !== null) {
