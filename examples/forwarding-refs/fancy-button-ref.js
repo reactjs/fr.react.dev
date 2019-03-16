a0420068dbcbfbd6ca3163102b4b4ffb62cd47bc
Christophe Porteneuve <tdd@tddsworld.com>
2019-03-16 14:38:22 +0100

    Final tweaks: example 2/4

--- examples/forwarding-refs/fancy-button-ref.js
+++ examples/forwarding-refs/fancy-button-ref.js
@@ -3,11 +3,10 @@ import FancyButton from './FancyButton';
 // highlight-next-line
 const ref = React.createRef();
 
-// Le composant FancyButton que nous avons importé est le HOC LogProps.
-// Même si l’affichage sera le même,
-// Notre ref visera LogProps au lieu du composant FancyButton !
-// Ça signifie par exemple que nous ne pouvons pas appeler ref.current.focus()
-// highlight-range{4}
+// Le composant FancyButton que nous avons importé est le HOC LogProps.  Même si
+// l’affichage sera le même, notre ref visera LogProps au lieu du composant
+// FancyButton !  Ça signifie par exemple que nous ne pouvons pas appeler
+// ref.current.focus() highlight-range{4}
 <FancyButton
   label="Cliquez ici"
   handleClick={handleClick}
