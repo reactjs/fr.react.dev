8a34647abd933f065ee1d8d0eb202509d656580e
Christophe Porteneuve <tdd@tddsworld.com>
2019-03-20 16:48:58 +0100

    Final tweaks: code example 2/4

--- examples/context/motivation-solution.js
+++ examples/context/motivation-solution.js
@@ -1,6 +1,6 @@
 // highlight-range{1-4}
-// Le Contexte nous permet de transmettre une prop profondément dans l’arbre des composants
-// sans la faire passer explicitement à travers tous les composants.
+// Le Contexte nous permet de transmettre une prop profondément dans l’arbre des
+// composants sans la faire passer explicitement à travers tous les composants.
 // Crée un contexte pour le thème (avec “light” comme valeur par défaut).
 const ThemeContext = React.createContext('light');
 
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     // highlight-range{1-3,5}
     // Utilise un Provider pour passer le thème plus bas dans l’arbre.
-    // N'importe quel composant peut le lire, quelle que soit sa profondeur.
+    // N’importe quel composant peut le lire, quelle que soit sa profondeur.
     // Dans cet exemple, nous passons “dark” comme valeur actuelle.
     return (
       <ThemeContext.Provider value="dark">
@@ -18,9 +18,8 @@ class App extends React.Component {
   }
 }
 
-// highlight-range{1,2}
-// Un composant au milieu n'a plus à
-// transmettre explicitement le thème
+// highlight-range{1,1}
+// Un composant au milieu n’a plus à transmettre explicitement le thème
 function Toolbar(props) {
   return (
     <div>
@@ -31,8 +30,8 @@ function Toolbar(props) {
 
 class ThemedButton extends React.Component {
   // highlight-range{1-3,6}
-  // Définit un contextType pour lire le contexte de thème actuel.
-  // React va trouver le Provider de thème ancêtre le plus proche et utiliser sa valeur.
+  // Définit un contextType pour lire le contexte de thème actuel.  React va
+  // trouver le Provider de thème ancêtre le plus proche et utiliser sa valeur.
   // Dans cet exemple, le thème actuel est “dark”.
   static contextType = ThemeContext;
   render() {
