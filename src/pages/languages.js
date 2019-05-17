dbb1ada1bc16a366e82288aaec02d399464ace03
Christophe Porteneuve <tdd@tddsworld.com>
2019-05-17 17:25:16 +0200

    Update languages.js

--- src/pages/languages.js
+++ src/pages/languages.js
@@ -11,13 +11,9 @@ import Container from 'components/Container';
 import Header from 'components/Header';
 import Layout from 'components/Layout';
 import React from 'react';
-<<<<<<< HEAD
 import TitleAndMetaTags from 'components/TitleAndMetaTags';
-=======
 import {urlRoot} from 'site-constants';
 import {media, sharedStyles} from 'theme';
-
->>>>>>> 06deefa56756f8dac13af59ee657186a81d28151
 // $FlowFixMe This is a valid path
 import languages from '../../content/languages.yml';
 
@@ -52,16 +48,11 @@ const Languages = ({location}: Props) => (
     <Container>
       <div css={sharedStyles.articleLayout.container}>
         <div css={sharedStyles.articleLayout.content}>
-<<<<<<< HEAD
-          <Header>Langues</Header>
-          <TitleAndMetaTags title="React - Langues" />
-=======
           <Header>Languages</Header>
           <TitleAndMetaTags
             canonicalUrl={`${urlRoot}/languages/`}
-            title="React - Languages"
+            title="React - Langues"
           />
->>>>>>> 06deefa56756f8dac13af59ee657186a81d28151
 
           <div css={sharedStyles.markdown}>
             <p>
